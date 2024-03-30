5be8338db5852286d1858b2ecbde52dd9bfc4ef7
RongGuang <424341023@qq.com>
2024-03-30 19:59:30 +0800

    Java核心技术卷I第12章

---
+++ v1ch12/src/threads/Bank.java
@@ -0,0 +1,39 @@
+package threads;
+
+import java.util.Arrays;
+
+/**
+ * @author rongguang
+ * @version 1.0.0
+ * @date 2024年03月30日 16:29:53
+ * @packageName threads
+ * @className Bank
+ */
+public class Bank {
+    private final double[] accounts;
+
+    public Bank(int n, double inicialBalance) {
+        accounts = new double[n];
+        Arrays.fill(accounts, inicialBalance);
+    }
+
+    public void transfer(int from, int to, double amount) {
+        if (accounts[from] < amount) return;
+        System.out.print(Thread.currentThread());
+        accounts[from] -= amount;
+        System.out.printf(" %10.2f from %d to %d", amount, from, to);
+        accounts[to] += amount;
+        System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
+    }
+
+    public double getTotalBalance() {
+        double sum = 0;
+        for (double a : accounts)
+            sum += a;
+        return sum;
+    }
+
+    public int size() {
+        return accounts.length;
+    }
+}
---
+++ v1ch12/src/threads/ThreadTest.java
@@ -0,0 +1,42 @@
+package threads;
+
+/**
+ * @author rongguang
+ * @version 1.0.0
+ * @date 2024年03月30日 16:37:31
+ * @packageName threads
+ * @className ThreadTest
+ */
+public class ThreadTest {
+    public static final int DELAY = 10;
+    public static final int STEPS = 100;
+    public static final double MAX_AMOUNT = 1000;
+
+    public static void main(String[] args) {
+        var bank = new Bank(4, 100000);
+        Runnable task1 = () -> {
+            try {
+                for (int i = 0; i < STEPS; i++) {
+                    double amount = MAX_AMOUNT * Math.random();
+                    bank.transfer(0, 1, amount);
+                    Thread.sleep((int) (DELAY * Math.random()));
+                }
+            } catch (InterruptedException e) {
+            }
+        };
+
+        Runnable task2 = () -> {
+            try {
+                for (int i = 0; i < STEPS; i++) {
+                    double amount = MAX_AMOUNT * Math.random();
+                    bank.transfer(2, 3, amount);
+                    Thread.sleep((int) (DELAY * Math.random()));
+                }
+            } catch (InterruptedException e) {
+            }
+        };
+
+        new Thread(task1).start();
+        new Thread(task2).start();
+    }
+}
